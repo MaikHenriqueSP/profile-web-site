651beef214b91db37417360b70f5fc005d2f1cc4
Maik Henrique <maik.hsp.contato@gmail.com>
2020-12-24 12:08:00 -0300

    feat(base project): creation of the needed base settings
    - creation of the needed stylesheets files
    - addition of node-sass dependency
    - creation of the base project folder structure
    - creation of the navbar component and stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+import { Navbar } from "./components";
+
+import "./style/scss/index.scss";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>React Appṕ</h1>
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Navbar = () => <h1>navbar</h1>;
+
+export default Navbar;
