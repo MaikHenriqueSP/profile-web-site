85ba91638f140a69f22c67b7ec095a21759e97da
Maik Henrique <maik.hsp.contato@gmail.com>
2020-12-24 14:31:55 -0300

    feat(navbar): base implementation
    - implementation of the base jsx
    - implementation of the base stylization

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import styles from "./Navbar.module.scss";
 
-const Navbar = () => <h1>navbar</h1>;
+const Navbar = () => (
+  <header className={styles.navbarContainer}>
+    <nav>
+      <ul className={styles.navLinksList}>
+        <li>
+          <a href="/home" className={styles.homeLink}>
+            HOME
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <nav>
+      <ul className={styles.navLinksList}>
+        <li>
+          <a href="/portfolio">PORTFOLIO</a>
+        </li>
+        <li>
+          <a href="/about-me">ABOUT ME</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
 
 export default Navbar;
