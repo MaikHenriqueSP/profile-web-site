dd83c7b9c595e160100fad3dd15a287b2d57e394
Maik Henrique <maik.hsp.contato@gmail.com>
2020-12-25 11:34:38 -0300

    feat(home): implementation of the base page
    - implementation of he base home page
    - implementation of the base stylization

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-const Home = () => <h1>Home Page</h1>;
+import styles from "./Home.module.scss";
+
+const Home = () => (
+  <div className={styles.container}>
+    <div className={styles.mainContent}>
+      <h1>
+        {"Hi, I'm "}
+        <span className={styles.nameHighlight}>Maik</span>
+      </h1>
+      <p>I’m a Computer Science student</p>
+      <p> and an aspiring developer</p>
+    </div>
+  </div>
+);
 
 export default Home;
