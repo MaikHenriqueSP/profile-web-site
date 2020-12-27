efe99e6bf841f45d0271b4f36ff357a5672a6c8d
Maik Henrique <maik.hsp.contato@gmail.com>
2020-12-27 12:05:40 -0300

    feat(about me): implementation of base stylization
    - download of the required svg icons
    - base stylization of the page

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import styles from "./AboutMe.module.scss";
 
-import profilePicture from "../../resources/img/profile-pic.jpeg";
+import {
+  profilePicture,
+  linkedinIcon,
+  githubIcon,
+  javaIcon,
+  javascriptIcon,
+  pythonIcon,
+  reactIcon,
+  sqlIcon,
+} from "../../resources/img/index";
 
 const AboutMe = () => (
   <section className={styles.container}>
-    <section className="profileDescription">
-      <section>
+    <section className={styles.profileDescriptionContainer}>
+      <section className={styles.profileDescripitionItem}>
         <header>
           <h2>About me</h2>
         </header>
@@ -20,7 +29,7 @@ const AboutMe = () => (
           </p>
         </article>
       </section>
-      <section>
+      <section className={styles.profileDescripitionItem}>
         <header>
           <h2>Fields of Interest</h2>
         </header>
@@ -32,32 +41,79 @@ const AboutMe = () => (
           </ul>
         </article>
       </section>
-      <section>
+      <section className={styles.profileDescripitionItem}>
         <header>
           <h2>Languages</h2>
         </header>
         <article>
-          <ul>
-            <li>Java</li>
-            <li>Java</li>
-            <li>Java</li>
-            <li>Java</li>
-            <li>Java</li>
+          <ul className={styles.knownLanguagesList}>
+            <li>
+              <img
+                src={javaIcon}
+                alt="Java icon"
+                className={styles.defaultIcon}
+              />
+              <span>Java</span>
+            </li>
+            <li>
+              <img
+                src={javascriptIcon}
+                alt="Javascript icon"
+                className={styles.defaultIcon}
+              />
+              <span>Javascript</span>
+            </li>
+            <li>
+              <img
+                src={pythonIcon}
+                alt="Python icon"
+                className={styles.defaultIcon}
+              />
+              <span>Python</span>
+            </li>
+            <li>
+              <img
+                src={reactIcon}
+                alt="React icon"
+                className={styles.defaultIcon}
+              />
+              <span>React</span>
+            </li>
+            <li>
+              <img
+                src={sqlIcon}
+                alt="SQL icon"
+                className={styles.defaultIcon}
+              />
+              <span>SQL</span>
+            </li>
           </ul>
         </article>
       </section>
     </section>
-    <section className="profileContact">
-      <article className="profilePicture">
+    <section className={styles.profileContactContainer}>
+      <article className={styles.profilePicture}>
         <img src={profilePicture} alt="Maik's profile" />
       </article>
-      <article className="professionalContact">
+      <article className={styles.professionalContact}>
         <ul>
           <li>
-            <a href="http://www.linkedin.com">Linkedin</a>
+            <a href="http://www.linkedin.com">
+              <img
+                src={linkedinIcon}
+                alt="Linkeding icon"
+                className={styles.defaultIcon}
+              />
+            </a>
           </li>
           <li>
-            <a href="http://www.github.com">Github</a>
+            <a href="http://www.github.com">
+              <img
+                src={githubIcon}
+                alt="Github icon"
+                className={styles.defaultIcon}
+              />
+            </a>
           </li>
         </ul>
       </article>
---
+++ src/resources/img/index.js
@@ -0,0 +1,11 @@
+export { default as linkedinIcon } from "./linkedin-icon.svg";
+export { default as githubIcon } from "./github-icon.svg";
+export { default as javaIcon } from "./java-icon.svg";
+export { default as javascriptIcon } from "./javascript-icon.svg";
+export { default as pythonIcon } from "./python-icon.svg";
+export { default as reactIcon } from "./react-icon.svg";
+export { default as sqlIcon } from "./sql-icon.svg";
+
+export { default as profilePicture } from "./profile-pic.jpeg";
+
+export { default as pencilBackground } from "./pencil.svg";
